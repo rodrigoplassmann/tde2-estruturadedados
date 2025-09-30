ada8e268ff9dcfae4e7dc497e19f5519530b4af0
Guilherme006 <guilherme_lazari@outlook.com>
2025-09-30 19:56:16 -0300

    Ajusta método 'busca' para adequar com as proibições

--- src/Trie.java
+++ src/Trie.java
@@ -16,14 +16,16 @@ public class Trie {
         atual.fimDaPalavra = true;
     }
 
-    public boolean busca(String palavra){
+    public boolean busca(String palavra) {
         NoTrie atual = raiz;
-        for(char c : palavra.toCharArray()){
-            if(atual.filho[c - 'a'] == null){
-                return false;
-            }
-            atual = atual.filho[c - 'a'];
+        for (int i = 0; i < palavra.length(); i++) {
+            char c = palavra.charAt(i);
+            int indice = c - 'a';
+            if (indice < 0 || indice >= 26) return false;
+            if (atual.filho[indice] == null) return false;
+            atual = atual.filho[indice];
         }
         return atual.fimDaPalavra;
     }
+
 }
